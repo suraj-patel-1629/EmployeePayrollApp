605e5d615f4ee7f9ae97849996a471ebd43b220f
suraj patel <sp9589890205@gmail.com>
2025-02-23 22:55:56 +0530

    [Suraj] add. setup for employee payroll system

--- src/main/java/com/bridgelabz/employeepayroll/model/Employee.java
+++ src/main/java/com/bridgelabz/employeepayroll/model/Employee.java
@@ -48,5 +48,12 @@ public class Employee {
         this.department = department;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
